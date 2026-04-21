8c31acf54698aca4092c07a89461879de5a0e483
Ramon R. <99776254+RamonSRibeiro@users.noreply.github.com>
2026-04-20 21:09:55 -0300

    Refactor URL configuration and add default path

--- config/urls.py
+++ config/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('usuarios/', include('usuarios.urls')),
     path('tarefas/', include('tarefas.urls')),
+    path('', include('tarefas.urls')),
 ]
